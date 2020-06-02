3b316d31f6a52586ca47e0dd7fb69cf8c4fd18a2
Alpha Tam <alpha.tam@welab.co>
2020-06-03 00:24:41 +0800

    Generate unique ssh keys

--- sftp/entrypoint.sh
+++ sftp/entrypoint.sh
@@ -3,6 +3,14 @@ set -e
  
 printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
  
+    # Generate unique ssh keys for this container, if needed
+    if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+        ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+    fi
+    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+        ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ''
+    fi
+    
 service ssh start
 service ssh status
  
