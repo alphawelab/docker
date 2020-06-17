ec2911bed3c1a71d04d754b18c261fa48198072c
Alpha Tam <alpha.tam@welab.co>
2020-06-17 12:17:03 +0800

    support pw and ssh key version

--- sftp/entrypoint.sh
+++ sftp/entrypoint.sh
@@ -4,21 +4,24 @@ set -e
 printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
  
     # Generate unique ssh keys for this container, if needed
-    if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
-        ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
-    fi
-    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-        ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ''
-    fi
-    if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
-        ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
-    fi
+    # if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+    #     ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+    # fi
 
+    # if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    #     ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ''
+    # fi
+
+    # if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
+    #     ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
+    # fi
 
 echo "\nCreate SSH User\n"
 sh /usr/local/bin/user.sh
 
-service ssh start
-service ssh status
+chmod 700 /opt
+
+# service ssh start
+# service ssh status
 
 exec "$@"
--- sftp/user.sh
+++ sftp/user.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 set -e
- 
+
 printf "\n\033[0;44m---> Creating SSH master user.\033[0m\n"
- 
-useradd -m -d /home/${SSH_MASTER_USER} -G ssh ${SSH_MASTER_USER} -s /bin/bash
-echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
-echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
 
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
+# Create user if not existed
+if ! id -u ${SSH_USER} > /dev/null 2>&1 ; then
+    useradd -m -G sftp ${SSH_USER} -s /usr/sbin/nologin
+    echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_USER}/.profile
+    mkdir -p /opt/${SSH_USER}/upload
+    chown ${SSH_USER}:${SSH_USER} /opt/${SSH_USER}/upload
+    chmod 700 /opt
+fi 
 
-addgroup sftp
+# Check if ssh password exist if ssh password authen is enabled
+if [ ! "${SSH_PASS}" ] && ! grep "PasswordAuthentication no" /etc/ssh/sshd_config >/dev/null ; then
+    echo "Environment Variable SSH_PASS is missing!" 1>&2
+    exit 1
+fi 
 
-# Add SSH keys to authorized_keys with valid permissions
-mkdir /home/${SSH_MASTER_USER}/.ssh
-chmod 700 /home/${SSH_MASTER_USER}/.ssh
-echo "$RSA_PUB_KEY" >> "/home/${SSH_MASTER_USER}/.ssh/authorized_keys"
-chown "${SSH_MASTER_USER}:sftp" "/home/${SSH_MASTER_USER}/.ssh/authorized_keys"
-chmod 600 "/home/${SSH_MASTER_USER}/.ssh/authorized_keys"
+# Add user password
+if [ "${SSH_PASS}" ]; then 
+    echo "${SSH_USER}:${SSH_PASS}" | chpasswd
+fi
+
+# Check if RSA_PUB_KEY is provided
+if [ ! "${RSA_PUB_KEY}" ] && grep "PasswordAuthentication no" /etc/ssh/sshd_config >/dev/null ; then
+    echo "Environment Variable RSA_PUB_KEY is missing!" 1>&2
+    exit 1
+fi 
+
+# Add public key to %HOME/.ssh/authorized_keys
+if [ "${RSA_PUB_KEY}" ]; then 
+
+    # Add random password to user
+    if [ ! "${SSH_PASS}" ]; then 
+        password=`tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1`
+        echo "${SSH_USER}:${password}" | chpasswd
+    fi
+    
+    mkdir /home/${SSH_USER}/.ssh
+    chmod 700 /home/${SSH_USER}/.ssh
+    chown ${SSH_USER}:${SSH_USER} /home/${SSH_USER}/.ssh
+    echo "${RSA_PUB_KEY}" >> "/home/${SSH_USER}/.ssh/authorized_keys"
+    chown ${SSH_USER}:${SSH_USER} "/home/${SSH_USER}/.ssh/authorized_keys"
+    chmod 600 "/home/${SSH_USER}/.ssh/authorized_keys"
+fi
 
 exec "$@"
